89424cbb2ba1f1b5ca051213a0523c0ecdc0d5fd
yaeraly <ya.eraly@gmail.com>
2020-06-24 00:23:22 +0300

    Linear Search Algorithm

--- Linear Search/linear.c
+++ Linear Search/linear.c
@@ -2,12 +2,15 @@
 
 int main(void)
 {
-    int target_num;
+    int target_num, length;
+
     printf("Search: ");
     scanf("%d", &target_num);
 
-    int numbers[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    for (int i = 0; i < 10; i++)
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    length = sizeof(numbers) / sizeof(int);
+
+    for (int i = 0; i < length; i++)
     {
         if (numbers[i] == target_num)
         {
